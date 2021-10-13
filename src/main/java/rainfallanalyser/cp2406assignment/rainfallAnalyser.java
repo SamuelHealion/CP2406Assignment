236579e6520940bcee923dc85d276a784464aea3
Samuel Healion <samuel.healion@my.jcu.edu.au>
2021-10-13 22:31:42 +1000

    Add calculator for monthly total rainfall

--- src/main/java/rainfallanalyser/cp2406assignment/rainfallAnalyser.java
+++ src/main/java/rainfallanalyser/cp2406assignment/rainfallAnalyser.java
@@ -28,34 +28,66 @@ public class rainfallAnalyser {
 
         System.out.println("\nWhich file would you like to analyse? Enter the corresponding number");
 
-        int fileNumber;
-        String fileName;
-        while (true) {
-            // Check that the selected file is valid. TextIO handles a non-Int input
-            try {
-                fileNumber = TextIO.getInt();
-                fileName = pathNames[fileNumber - 1];
-                break;
-            }
-            catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("That is outside of the range of available data files to analyse.");
-                System.out.println("Please choose another one");
-            }
-        }
+//        int fileNumber;
+//        String fileName;
+//        while (true) {
+//            // Check that the selected file is valid. TextIO handles a non-Int input
+//            try {
+//                fileNumber = TextIO.getInt();
+//                fileName = pathNames[fileNumber - 1];
+//                break;
+//            }
+//            catch (ArrayIndexOutOfBoundsException e) {
+//                System.out.println("That is outside of the range of available data files to analyse.");
+//                System.out.println("Please choose another one");
+//            }
+//        }
+
+        int currentYear = 0, currentMonth = 0;
+
+        double monthlyTotal = 0;
 
         try {
-            Reader reader = new FileReader("./rainfalldata/" + fileName);
+//            Reader reader = new FileReader("./rainfalldata/" + fileName);
+            Reader reader = new FileReader("./rainfalldata/MountSheridanStationCNS.csv");
 //            System.out.println(pathNames[fileNumber - 1]);
             Iterable<CSVRecord> records = CSVFormat.DEFAULT.withHeader("Product Code", "Bureau of Meteorology station number", "Year", "Month", "Day",
                     "Rainfall", "Period of Measurement", "Quality").parse(reader);
             for (CSVRecord record : records) {
                 // Get the data from each row of the Rainfall Data CSV file
-                String year = record.get("Year");
-                String month = record.get("Month");
-                String day = record.get("Day");
-                String rainfall = record.get("Rainfall");
-                System.out.println(day+"/"+month+"/"+year + " received " + rainfall + " millimeters of rain");
+                String yearText = record.get("Year");
+                String monthText = record.get("Month");
+                String dayText = record.get("Day");
+                String rainfallText = record.get("Rainfall");
+//                System.out.println(dayText+"/"+monthText+"/"+yearText + " received " + rainfallText + " millimeters of rain");
+
+                try {
+                    if (currentYear != Integer.parseInt(yearText)) {
+                        System.out.println("Total rainfall for " + currentMonth +"/"+ yearText + " is: " + monthlyTotal + " millimeters");
+                        currentYear = Integer.parseInt(yearText);
+                        monthlyTotal = 0;
+                    } else {
+                        if (currentMonth != Integer.parseInt(monthText)) {
+                            System.out.println("Total rainfall for " + currentMonth +"/"+ yearText + " is: " + monthlyTotal + " millimeters");
+                            currentMonth = Integer.parseInt(monthText);
+                            monthlyTotal = 0;
+                        }
+                        try {
+                            monthlyTotal += Double.parseDouble(rainfallText);
+//                            System.out.println(Double.parseDouble(rainfallText));
+                        } catch (NumberFormatException e) {
+//                                System.out.println("No rainfall data");
+                        }
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("This particular error");
+                    System.out.println(e.getMessage());
+                    System.out.println(yearText);
+                    System.out.println(monthText);
+                }
             }
+//            System.out.println(monthlyTotal);
+
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
