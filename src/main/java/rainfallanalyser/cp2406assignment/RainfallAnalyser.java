3d17eb686b137f9fc3ef602051bd2b141846b15c
Samuel Healion <samuel.healion@my.jcu.edu.au>
2021-10-14 18:02:24 +1000

    Refactor program to extract methods

--- src/main/java/rainfallanalyser/cp2406assignment/RainfallAnalyser.java
+++ src/main/java/rainfallanalyser/cp2406assignment/RainfallAnalyser.java
@@ -15,55 +15,37 @@ public class RainfallAnalyser {
      */
     public static void main(String[] args) {
 
+        System.out.println("Welcome to the Rainfall Analyser");
         System.out.println("This program analyses the rainfall data given from various sources like BOM");
-        System.out.println("The files available are:\n");
+        System.out.println("It will then return the extracted Total Monthly Rainfall for the data set");
+        System.out.println("as well as the minimum and maximum rainfall that occurred that month.");
 
-        File f = new File("./rainfalldata");
-        String[] pathNames = f.list();
 
-        assert pathNames != null;
-        for (int i = 0; i < pathNames.length; i++) {
-            System.out.println((i+1) + ": " + pathNames[i]);
-        }
+//        String filename = getRainfallData();
+        String filename = "MountSheridanStationCNS.csv";
+        analyseRainfallData(filename);
+    }
 
-        System.out.println("\nWhich file would you like to analyse? Enter the corresponding number");
-
-//        int fileNumber;
-//        String fileName;
-//        while (true) {
-//            // Check that the selected file is valid. TextIO handles a non-Int input
-//            try {
-//                fileNumber = TextIO.getInt();
-//                fileName = pathNames[fileNumber - 1];
-//                break;
-//            }
-//            catch (ArrayIndexOutOfBoundsException e) {
-//                System.out.println("That is outside of the range of available data files to analyse.");
-//                System.out.println("Please choose another one");
-//            }
-//        }
-
-        int currentYear = 0, currentMonth = 0;
-
+    private static void analyseRainfallData(String fileName) {
+        int currentYear = 0;
+        int currentMonth = 0;
         double monthlyTotal = 0.0;
         double minRainfall = Double.POSITIVE_INFINITY;
         double maxRainfall = 0.0;
 
         try {
-//            Reader reader = new FileReader("./rainfalldata/" + fileName);
-            Reader reader = new FileReader("./rainfalldata/MountSheridanStationCNS.csv");
-//            System.out.println(pathNames[fileNumber - 1]);
-
+            Reader reader = new FileReader("./rainfalldata/" + fileName);
             Iterable<CSVRecord> records = CSVFormat.DEFAULT.withHeader("Product Code", "Bureau of Meteorology station number", "Year", "Month", "Day",
-                    "Rainfall", "Period of Measurement", "Quality").parse(reader);
+                    "Rainfall", "Period of Measurement", "Quality").withSkipHeaderRecord().parse(reader);
+
             for (CSVRecord record : records) {
                 // Get the data from each row of the Rainfall Data CSV file
                 String yearText = record.get("Year");
                 String monthText = record.get("Month");
                 String dayText = record.get("Day");
                 String rainfallText = record.get("Rainfall");
-//                System.out.println(dayText+"/"+monthText+"/"+yearText + " received " + rainfallText + " millimeters of rain");
 
+                // Analyse the data and convert it into the expected output format
                 try {
                     int year = Integer.parseInt(yearText);
                     int month = Integer.parseInt(monthText);
@@ -71,8 +53,10 @@ public class RainfallAnalyser {
                     double rainfall = Double.parseDouble(rainfallText);
 
                     if (month != currentMonth) {
-                        System.out.println("Total rainfall for " + currentMonth + "/" + yearText + " is: " + monthlyTotal + " millimeters");
-                        System.out.println("Maximum rainfall was: " + maxRainfall + " and Minimum rainfall was: " + minRainfall);
+//                        System.out.println("Total rainfall for " + currentMonth + "/" + yearText + " is: " + monthlyTotal + " millimeters");
+//                        System.out.println("Maximum rainfall was: " + maxRainfall + " and Minimum rainfall was: " + minRainfall);
+                        System.out.printf("%d,%d,%1.2f,%1.2f,%1.2f\n", year, month, monthlyTotal, minRainfall, maxRainfall);
+
                         currentYear = year;
                         currentMonth = month;
                         monthlyTotal = 0;
@@ -86,35 +70,8 @@ public class RainfallAnalyser {
                             minRainfall = rainfall;
                     }
                 } catch (NumberFormatException e) {
-//                    System.out.println("Error");
+                    System.out.println("Error");
                 }
-
-//                try {
-//                    if (currentYear != Integer.parseInt(yearText)) {
-//                        currentYear = Integer.parseInt(yearText);
-//                        currentMonth = Integer.parseInt(monthText);
-//                        monthlyTotal = 0;
-//                    } else {
-//                        if (currentMonth != Integer.parseInt(monthText)) {
-//                            System.out.println("Total rainfall for " + currentMonth +"/"+ yearText + " is: " + monthlyTotal + " millimeters");
-//                            currentMonth = Integer.parseInt(monthText);
-//                            monthlyTotal = 0;
-//                        }
-//                        try {
-//
-//                            monthlyTotal += Double.parseDouble(rainfallText);
-////                            System.out.println(Double.parseDouble(rainfallText));
-//                        } catch (NumberFormatException e) {
-//                            // When there is no number in the data (or is an invalid string) treat it as
-//                            // a zero entry and continue with parsing the file
-////                                System.out.println("No rainfall data");
-//                        }
-//                    }
-//                } catch (NumberFormatException e) {
-//                    System.out.println("Error converting to a number.");
-//                    // TODO Work out how to parse the first line without triggering this error
-//                    System.out.println(e.getMessage());
-//                }
             }
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
@@ -122,5 +79,39 @@ public class RainfallAnalyser {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Get a list of available rainfall data sets to be analysed.
+     * Allows the user to pick which data set to analyse from this list.
+     * @return Name of the file to be analysed
+     */
+    private static String getRainfallData() {
+        System.out.println("The files available are:\n");
+        File f = new File("./rainfalldata");
+        String[] pathNames = f.list();
+
+        assert pathNames != null;
+        for (int i = 0; i < pathNames.length; i++) {
+            System.out.println((i+1) + ": " + pathNames[i]);
+        }
+
+        System.out.println("\nWhich file would you like to analyse? Enter the corresponding number");
+
+        int fileNumber;
+        String fileName;
+        while (true) {
+            // Check that the selected file is valid. TextIO handles a non-Int input
+            try {
+                fileNumber = TextIO.getInt();
+                fileName = pathNames[fileNumber - 1];
+                break;
+            }
+            catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("That is outside of the range of available data files to analyse.");
+                System.out.println("Please choose another one");
+            }
+        }
+        return fileName;
     }
 }
